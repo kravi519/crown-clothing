1debf670219588ddf365ed0580928f1eb2f4980d
ravikumar <kravi519@gmail.com>
2021-09-02 00:25:20 +0530

    user redirect and user action

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
@@ -37,14 +37,18 @@ class App extends React.Component {
        <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
-          <Route path='/signIn' component={SignInSignUpPage} />
+          <Route exact path='/signIn' render={() => 
+            this.props.currentUser ? (<Redirect to='/'/>):(<SignInSignUpPage/>)} />
         </Switch> 
       </div>
     );
   }  
 }
 
+const mapStateToProps = ({ user }) =>({
+  currentUser: user.currentUser
+})
 const mapDispatchToProps = dispatch =>({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
