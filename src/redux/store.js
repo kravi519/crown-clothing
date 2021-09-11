77e90d38f044f980757a871ca4128a6ee6e80102
ravikumar <kravi519@gmail.com>
2021-09-11 18:14:54 +0530

    logger change

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,10 @@ import rootReducer from "./root-reducer";
 import { persistStore } from 'redux-persist';
 const middleWares = [logger];
 
+if (process.env.NODE_ENV === 'development'){
+    middleWares.push(logger);
+}
+
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
 export const persistor = persistStore(store);
